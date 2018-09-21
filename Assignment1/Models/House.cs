dd6f3641d014bcc6613698f216688c5fbbc98377
Foyaz <kayserfoyez68@gmail.com>
2018-09-21 09:47:58 -0500

    first commit

---
+++ Assignment1/Models/House.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1.Models
+{
+    public class House
+    {
+        public int Value { get; set; }
+        public int LotSize { get; set; }
+        public String Address { get; set; }
+        public String City { get; set; }
+        public String State { get; set; }
+        public String PostalCode { get; set; }
+    }
+}
